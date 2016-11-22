3df4cfd444c0cf2d6a526038277ee293e20e0ffb
Administrator <Administrator@WIN-ET4M2AH4BNR>
2016-11-22 12:59:26 +0000

    プロジェクト ファイルを追加します。

---
+++ DiffForMzPlatform/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.IO;
+
+namespace DiffForMzPlatform
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MzXmlParser xmlPerser = new MzXmlParser();
+            xmlPerser.SplitInvocations(args[0]);
+
+        }
+    }
+
+    class MzXmlParser
+    {
+        // XMLを読み取りファイルに出力する
+        public void SplitInvocations(string filename)
+        {
+            XDocument xmlDoc = XDocument.Load(filename);
+            foreach (var invocation in  xmlDoc.Element("information").Element("invocations").Elements("invocation")){
+                // コンポーネントIDを取得
+                var srcComponentId = invocation.Element("source").Attribute("component").Value.ToString();
+
+                // ファイル名に変換
+                Directory.CreateDirectory("splitfiles");
+                File.WriteAllText("splitfiles\\component_" + srcComponentId + ".xml", invocation.ToString());
+
+            }
+
+        }
+
+    }
+}
